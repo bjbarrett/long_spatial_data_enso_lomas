53f7ae11f9c14a791c7f2b0b3fe9b6763398f490
Brendan Barrett <bbarrett@ab.mpg.de>
2023-10-05 11:07:50 +0200

    added bret dynamic calc code

---
+++ 05_dynamic_calcs.R
@@ -0,0 +1,24 @@
+
+prep_latex_variables <- function(named_list) {
+  out <- character()
+  for (i in 1:length(named_list)) {
+    out[i] <- paste0("\\newcommand{\\", names(named_list)[i], "}{", named_list[[i]], "}")
+  }
+  return(out)
+}
+
+calcs <- list(
+  nObs = prettyNum(nrow(obs), big.mark = ","), # assuming an `obs` data frame
+  nPpl = nrow(ppl),                            # assuming a `ppl` data frame
+  meanHeight = round(mean(obs$height), 1)
+)
+
+writeLines(prep_latex_variables(calcs), "calcs.tex")
+
+# put this in the tex file's preamble:
+# \input{calcs.tex}
+
+# now can write LaTeX things like
+# "the number of observations is \nObs,
+# the number of people is \nPpl,
+# the mean height is \meanHeight."
